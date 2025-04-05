2d014d2bd57e804f551fe23dcf1569d360853c28
Adrian Galicia <adrian.galicia92@gmail.com>
2025-04-05 03:48:38 -0500

    changes for search without javascript - p1

---
+++ page-search.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+display_page_banner()
+?>
+
+
+<div class="container">
+    <!-- page content -->
+    <div class="generic-content">
+        <?php get_search_form(); ?>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form class="search-form" action="<?php echo esc_url(site_url('/')); ?>" method="GET" autocomplete="off">
+    <label class="results-heading" for="searchBox">Search for courses, events, teachers, blog posts and more..</label>
+    <div class="box-4">
+        <input class="search-input" type="search" id="searchBox" autofocus placeholder="What are you looking for?" name="s">
+        <input class="search-submit" type="submit" value="Search">
+    </div>
+</form>
